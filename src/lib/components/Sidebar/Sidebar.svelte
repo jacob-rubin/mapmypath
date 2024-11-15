eb66ee7ceb71763b4bfae71f131f6af366ee4821
jacob-rubin <jacob.rubin18@gmail.com>
2024-11-15 10:39:57 -0600

    feat: change sidebar and icon size

--- src/lib/components/Sidebar/Sidebar.svelte
+++ src/lib/components/Sidebar/Sidebar.svelte
@@ -3,5 +3,5 @@
 </script>
 
 <div class="card h-full w-full bg-orange-400">
-	<MdiArrowCollapseLeft class="absolute top-0 h-10 w-10 p-2 text-white" />
+	<MdiArrowCollapseLeft class="absolute top-0 size-10 p-2 text-white" />
 </div>
