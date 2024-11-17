a9eb41e0abb8e8c68de2da2d6bcc415ae1e08d80
jacob-rubin <113300226+jacob-rubin@users.noreply.github.com>
2024-11-16 20:30:34 -0600

    2 add collapsible sidebar component (#8)

---
+++ src/lib/components/Sidebar/Sidebar.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import MdiMenu from '~icons/mdi/menu';
+	import { slide, fly } from 'svelte/transition';
+
+	let isOpen: boolean = true;
+
+	function toggleSidebar() {
+		isOpen = !isOpen;
+	}
+</script>
+
+{#snippet card(width: string)}
+	<div data-testid="sidebar" class="card h-full {width} bg-neutral-content">
+		<button data-testid="sidebar-button" class="btn btn-ghost" onclick={toggleSidebar}>
+			<MdiMenu class="text-neutral" />
+		</button>
+	</div>
+{/snippet}
+
+<div data-testid="sidebar-div" class="h-screen p-2">
+	{#if isOpen}
+		{@render card('w-72')}
+	{:else}
+		{@render card('w-min')}
+	{/if}
+</div>
---
+++ src/lib/components/Sidebar/Sidebar.test.ts
@@ -0,0 +1,44 @@
+import { expect, test } from 'vitest';
+import { render } from 'vitest-browser-svelte';
+import Sidebar from './Sidebar.svelte';
+
+test('it should start out expanded', async () => {
+	const { getByTestId } = render(Sidebar);
+
+	const sidebar = getByTestId('sidebar');
+	expect(sidebar.element()).toHaveClass('w-72');
+});
+
+test('it should close when button clicked', async () => {
+	const { getByTestId } = render(Sidebar);
+
+	const button = getByTestId('sidebar-button');
+	await button.click();
+
+	const sidebar = getByTestId('sidebar');
+
+	expect(sidebar.element()).toHaveClass('w-min');
+});
+
+test('it should reopen when clicked in close state', async () => {
+	const { getByTestId } = render(Sidebar);
+
+	// Click the button twice to simulate opening and closing
+	const button = getByTestId('sidebar-button');
+	await button.click();
+	await button.click();
+
+	const sidebar = getByTestId('sidebar');
+
+	expect(sidebar.element()).toHaveClass('w-72');
+});
+
+// It should span the whole height of the screen, minues the padding of 8px
+test('it should span the whole height of the screen', async () => {
+	const { getByTestId } = render(Sidebar);
+
+	const sidebar = getByTestId('sidebar');
+	const sidebarHeight: number = sidebar.element().getBoundingClientRect().height;
+
+	expect(sidebarHeight).toBe(window.innerHeight - 16);
+});
