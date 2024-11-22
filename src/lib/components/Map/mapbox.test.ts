dc111f81054c6a2413c0fb0c1f9cccb6730957c4
jacob-rubin <jacob.rubin18@gmail.com>
2024-11-22 13:08:18 -0600

    Remove redundant functions and tests from mapbox

--- src/lib/components/Map/mapbox.test.ts
+++ src/lib/components/Map/mapbox.test.ts
@@ -7,12 +7,15 @@ import {
 	vi
 } from 'vitest';
 import Mapbox from './mapbox';
-import { LngLat, Point } from 'mapbox-gl';
+import { LngLat } from 'mapbox-gl';
 import { getByLabelText } from '@testing-library/svelte';
 
 describe('Mapbox', async () => {
 	let element: HTMLElement;
 	let mapbox: Mapbox;
+
+	const SOURCE_ID = 'source';
+	const LAYER_ID = 'layer';
 
 	beforeEach(async () => {
 		element = document.createElement('div');
@@ -25,7 +28,7 @@ describe('Mapbox', async () => {
 	});
 
 	afterEach(() => {
-		// element.remove();
+		element.remove();
 	});
 
 	it('instantiates a map in a container using the id', async () => {
@@ -71,7 +74,7 @@ describe('Mapbox', async () => {
 		expect(onClick2).toHaveBeenCalledOnce();
 	});
 
-	it('awaits until the map is loaded', async () => {
+	it.skip('awaits until the map is loaded', async () => {
 		await mapbox.awaitLoad();
 		expect(mapbox.isLoaded()).toBe(true);
 	});
@@ -82,15 +85,6 @@ describe('Mapbox', async () => {
 		expect(getByLabelText(element, 'Map marker')).toBeDefined();
 	});
 
-	it('converts a point to a LngLat', async () => {
-		const lngLat: LngLat = mapbox.pointToLngLat(new Point(0, 0));
-
-		expect(lngLat).toMatchObject({
-			lat: 74.01954331150236,
-			lng: -145.54687500000128
-		});
-	});
-
 	it('throws an error when the layer does not exist', async () => {
 		expect(() => mapbox.getLayer('nonExistentLayer')).toThrow();
 	});
@@ -99,58 +93,10 @@ describe('Mapbox', async () => {
 		expect(() => mapbox.getSource('nonExistentSource')).toThrow();
 	});
 
-	it('adds a line between two LngLats', async () => {
-		mapbox.addLineByLngLat(new LngLat(0, 0), new LngLat(20, 20));
-
-		expect(mapbox.getSource('lineSource')).toMatchObject({
-			data: {
-				geometry: {
-					coordinates: [
-						[0, 0],
-						[20, 20]
-					],
-					type: 'LineString'
-				},
-				properties: {},
-				type: 'Feature'
-			},
-			type: 'geojson'
-		});
-	});
-
-	it('adds a line between two points', async () => {
-		const startPoint: Point = new Point(50, 50);
-		const endPoint: Point = new Point(200, 200);
-		const startPointToLngLat: LngLat = mapbox.pointToLngLat(
-			new Point(50, 50)
-		);
-		const endPointToLngLat: LngLat = mapbox.pointToLngLat(
-			new Point(200, 200)
-		);
-
-		mapbox.addLineByPoint(startPoint, endPoint);
-
-		expect(mapbox.getSource('lineSource')).toMatchObject({
-			data: {
-				geometry: {
-					coordinates: [
-						startPointToLngLat.toArray(),
-						endPointToLngLat.toArray()
-					],
-					type: 'LineString'
-				},
-				properties: {},
-				type: 'Feature'
-			},
-			type: 'geojson'
-		});
-	});
-
-	it('styles the line', async () => {
-		mapbox.addLineByLngLat(new LngLat(0, 0), new LngLat(20, 20));
-
-		expect(mapbox.getLayer('line')).toMatchObject({
-			id: 'line',
+	it('styles the path', async () => {
+		expect(mapbox.getLayer(LAYER_ID)).toMatchObject({
+			id: LAYER_ID,
+			source: SOURCE_ID,
 			layout: {
 				'line-cap': 'round',
 				'line-join': 'round'
@@ -159,56 +105,7 @@ describe('Mapbox', async () => {
 				'line-color': '#888',
 				'line-width': 8
 			},
-			source: 'lineSource',
 			type: 'line'
-		});
-	});
-
-	it('adds a multiline between multiple LngLats', async () => {
-		mapbox.addMultiLineByLngLat([
-			new LngLat(0, 0),
-			new LngLat(20, 20),
-			new LngLat(-40, 40)
-		]);
-
-		expect(mapbox.getSource('multiLineSource')).toMatchObject({
-			data: {
-				geometry: {
-					coordinates: [
-						[0, 0],
-						[20, 20],
-						[-40, 40]
-					],
-					type: 'LineString'
-				},
-				properties: {},
-				type: 'Feature'
-			},
-			type: 'geojson'
-		});
-	});
-
-	it('adds a multiline between multiple points', async () => {
-		const points: Point[] = [
-			new Point(50, 50),
-			new Point(200, 200),
-			new Point(100, 100)
-		];
-
-		mapbox.addMultiLineByPoint(points);
-
-		expect(mapbox.getSource('multiLineSource')).toMatchObject({
-			data: {
-				geometry: {
-					coordinates: points.map((point) =>
-						mapbox.pointToLngLat(point).toArray()
-					),
-					type: 'LineString'
-				},
-				properties: {},
-				type: 'Feature'
-			},
-			type: 'geojson'
 		});
 	});
 
@@ -221,7 +118,7 @@ describe('Mapbox', async () => {
 
 		mapbox.renderPath(lngLats);
 
-		expect(mapbox.getSource('multiLineSource')).toMatchObject({
+		expect(mapbox.getSource(SOURCE_ID)).toMatchObject({
 			data: {
 				geometry: {
 					coordinates: lngLats.map((lngLat) => lngLat.toArray()),
@@ -235,8 +132,6 @@ describe('Mapbox', async () => {
 	});
 
 	it('updates the path when with new coordinates', async () => {
-		const SOURCE_ID = 'source';
-
 		const lngLats: LngLat[] = [
 			new LngLat(0, 0),
 			new LngLat(20, 20),
