ac6a532ffc25ce0814dca6c1e0333ca5fc21f559
jacob-rubin <jacob.rubin18@gmail.com>
2024-12-01 16:43:43 -0600

    Add sidebar item and test

--- src/lib/components/Sidebar/Sidebar.svelte
+++ src/lib/components/Sidebar/Sidebar.svelte
@@ -4,8 +4,8 @@
 	import { mapState } from '$lib/shared/mapState/mapState.svelte';
 
 	// TODO: Remove coupling
-	let isOpen: boolean = false;
-	let cardWidth: string = 'w-10';
+	let isOpen: boolean = true;
+	let cardWidth: string = 'w-72';
 
 	function toggleSidebar() {
 		isOpen = !isOpen;
@@ -13,38 +13,16 @@
 	}
 </script>
 
-<div data-testid="sidebar" class="h-screen p-2">
+<div class="h-screen p-2">
 	<div
+		data-testid={'sidebar'}
 		class="card h-full {cardWidth} bg-neutral-content transition-all"
 	>
-		<button
-			data-testid="sidebar-button"
-			class="btn btn-ghost p-0"
-			onclick={toggleSidebar}
-		>
+		<button class="btn btn-ghost p-0" onclick={toggleSidebar}>
 			<MdiMenu class="size-5 text-neutral" />
 		</button>
-		<div class="divider"></div>
-		{#if isOpen}
-			<div class="overflow-y-auto">
-				{#each mapState.getMarkers() as marker}
-					<div
-						class="card m-2 border-2 border-solid border-black p-2"
-					>
-						{marker.lngLat.lng}, {marker.lngLat.lat}
-					</div>
-				{/each}
-			</div>
-		{:else}
-			<div class="self-center overflow-hidden hover:overflow-y-auto">
-				{#each mapState.getMarkers() as marker, index}
-					<MdiLocation
-						data-testid={`marker${index}`}
-						class="size-8 text-neutral"
-					/>
-					<div class="divider"></div>
-				{/each}
-			</div>
-		{/if}
+		{#each mapState.getMarkers() as marker}
+			{marker.lngLat.lng}, {marker.lngLat.lat}
+		{/each}
 	</div>
 </div>
