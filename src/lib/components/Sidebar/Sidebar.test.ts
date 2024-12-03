34dc913a1fef1aa6de171b919528b73b7046c9a0
jacob-rubin <jacob.rubin18@gmail.com>
2024-12-02 20:33:39 -0600

    Add tests

--- src/lib/components/Sidebar/Sidebar.test.ts
+++ src/lib/components/Sidebar/Sidebar.test.ts
@@ -22,16 +22,16 @@ describe('Sidebar', async () => {
 
 	it('collapses when button clicked', async () => {
 		const screen = render(Sidebar);
-		const button: Locator = screen.getByTestId('sidebar-button');
+		const button: Locator = screen.getByRole('button');
 		await button.click();
 		const sidebar: Locator = screen.getByTestId('sidebar');
 
-		expect(sidebar.element()).toHaveClass('w-min');
+		expect(sidebar.element()).toHaveClass('w-10');
 	});
 
 	it('reexpands when the button is clicked while collapsed', async () => {
 		const screen = render(Sidebar);
-		const button: Locator = screen.getByTestId('sidebar-button');
+		const button: Locator = screen.getByRole('button');
 		await button.click();
 		await button.click();
 		const sidebar: Locator = screen.getByTestId('sidebar');
@@ -56,26 +56,5 @@ describe('Sidebar', async () => {
 		await tick();
 
 		expect(sidebar.element()).toHaveTextContent('0, 0');
-	});
-
-	// when collapsed, the sidebar should show a list of location markers
-	it('shows a list of three location markers when there are three markers and collapsed', async () => {
-		const screen = render(Sidebar);
-		const sidebar: Locator = screen.getByTestId('sidebar');
-
-		mapState.addMarker({ id: 0, lngLat: new LngLat(0, 0) });
-		mapState.addMarker({ id: 1, lngLat: new LngLat(1, 1) });
-		mapState.addMarker({ id: 2, lngLat: new LngLat(2, 2) });
-
-		await tick();
-
-		// Location marker elements should be present
-		const marker1: Locator = sidebar.getByTestId('marker0');
-		const marker2: Locator = sidebar.getByTestId('marker1');
-		const marker3: Locator = sidebar.getByTestId('marker2');
-
-		expect(marker1.query()).not.toBeNull();
-		expect(marker2.query()).not.toBeNull();
-		expect(marker3.query()).not.toBeNull();
 	});
 });
