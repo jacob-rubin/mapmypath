20e7ede229d7683859bb983cd88f29e40710d3bf
Jacob-Rubin-VTNC34_moto <jacob.rubin@motorolasolutions.com>
2024-12-26 14:37:45 -0600

    Replace slide with fly

--- src/lib/components/Sidebar/Sidebar.svelte
+++ src/lib/components/Sidebar/Sidebar.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	import MdiKeyboardArrowLeft from '~icons/mdi/keyboard-arrow-left';
 	import MdiKeyboardArrowRight from '~icons/mdi/keyboard-arrow-right';
-	import { slide } from 'svelte/transition';
+	import { fly } from 'svelte/transition';
 	import SidebarItem from '../SidebarItem/SidebarItem.svelte';
 	import { mapState } from '$lib/shared/mapState/mapState.svelte';
+	import { cubicOut } from 'svelte/easing';
 
 	let sidebar: HTMLDivElement | null = $state(null);
 
@@ -23,20 +24,7 @@
 	});
 </script>
 
-<div class="flex h-screen w-80 flex-grow flex-row items-center py-2">
-	{#if isOpen}
-		<div
-			bind:this={sidebar}
-			data-testid={'sidebar'}
-			class="card card-normal h-full w-full overflow-auto bg-neutral-content p-2"
-			transition:slide={{ axis: 'x' }}
-		>
-			{#each mapState.getMarkers() as marker (marker.id)}
-				<SidebarItem {marker} />
-			{/each}
-		</div>
-	{/if}
-
+{#snippet sidebarButton()}
 	<div
 		data-testid={'tooltip'}
 		class="tooltip tooltip-right flex"
@@ -59,4 +47,31 @@
 			{/if}
 		</button>
 	</div>
-</div>
+{/snippet}
+
+{#if isOpen}
+	<div
+		class="flex h-screen items-center py-2"
+		transition:fly={{
+			x: '-20rem',
+			opacity: 100,
+			easing: cubicOut
+		}}
+	>
+		<div
+			bind:this={sidebar}
+			data-testid={'sidebar'}
+			class="card card-normal h-full w-80 overflow-auto bg-neutral-content p-2"
+		>
+			{#each mapState.getMarkers() as marker (marker.id)}
+				<SidebarItem {marker} />
+			{/each}
+		</div>
+		{@render sidebarButton()}
+	</div>
+{:else}
+	<div class="flex h-screen items-center">
+		<div class="h-full"></div>
+		{@render sidebarButton()}
+	</div>
+{/if}
