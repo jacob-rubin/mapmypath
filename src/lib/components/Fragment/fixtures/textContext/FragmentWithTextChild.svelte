aad71cf7dfe0db46204615c3621e95323dc1d37a
jacob-rubin <jacob.rubin18@gmail.com>
2025-01-03 20:46:44 -0600

    Fix fragment test

--- src/lib/components/Fragment/fixtures/textContext/FragmentWithTextChild.svelte
+++ src/lib/components/Fragment/fixtures/textContext/FragmentWithTextChild.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
 	import Fragment from '../../Fragment.svelte';
 	import TextChild from './TextChild.svelte';
-
-	const textKey = Symbol('text');
 </script>
 
-<Fragment context={{ textKey: 'Hello World!' }}>
+<Fragment context={{ text: 'Hello World!' }}>
 	<TextChild></TextChild>
 </Fragment>
