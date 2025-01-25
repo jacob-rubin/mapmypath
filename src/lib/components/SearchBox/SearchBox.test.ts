ca536f82659ef09f5d565f41e97998b3b5a6a2b0
jacob-rubin <jacob.rubin18@gmail.com>
2025-01-25 13:53:24 -0600

    Add searchbox fixture

--- src/lib/components/SearchBox/SearchBox.test.ts
+++ src/lib/components/SearchBox/SearchBox.test.ts
@@ -1,4 +1,11 @@
-import { afterEach, describe, expect, it, vi } from 'vitest';
+import {
+	afterEach,
+	beforeEach,
+	describe,
+	expect,
+	it,
+	vi
+} from 'vitest';
 import SearchBox from './SearchBox.svelte';
 import {
 	cleanup,
@@ -9,8 +16,31 @@ import {
 import userEvent from '@testing-library/user-event';
 import { mockSuggestion } from '$lib/utils/searcher/mocks/mockSuggestion';
 import { tick } from 'svelte';
+import SearchboxFixture from './fixtures/SearchboxFixture.svelte';
+import Mapbox from '$lib/utils/mapbox/mapbox';
+import { MapState } from '$lib/state/mapState/mapState.svelte';
+import { mockFeature } from '$lib/utils/searcher/mocks/mockFeature';
+
+function renderMap(): Mapbox {
+	const mapElement: HTMLElement = document.createElement('div');
+	mapElement.innerHTML = `<div id="map" class="h-80 w-80"></div`;
+	document.body.appendChild(mapElement);
+
+	const map: Mapbox = new Mapbox(mapElement);
+	return map;
+}
 
 describe('SearchBox', async () => {
+	let map: Mapbox;
+	let mapState: MapState;
+
+	beforeEach(async () => {
+		map = renderMap();
+		mapState = new MapState(map);
+		await map.awaitLoad();
+		await map.initializeStyles();
+	});
+
 	afterEach(() => {
 		cleanup();
 	});
@@ -88,24 +118,43 @@ describe('SearchBox', async () => {
 		).not.toBeInTheDocument();
 	});
 
-	it('pastes the suggestion name into the searchbox when clicked', async () => {
+	it.only('pastes the suggestion name into the searchbox when clicked', async () => {
 		const user = userEvent.setup();
-		const mockFetch = vi.fn().mockResolvedValue({
+		const mockSuggestFetch = vi.fn().mockResolvedValue({
 			json: vi.fn().mockResolvedValue(mockSuggestion),
 			ok: true,
 			status: 200,
 			headers: { 'Content-Type': 'application/json' }
 		});
-		vi.stubGlobal('fetch', mockFetch);
+		vi.stubGlobal('fetch', mockSuggestFetch);
 
-		const screen = render(SearchBox);
+		const screen = render(SearchboxFixture, {
+			props: {
+				context: {
+					mapState: mapState
+				}
+			}
+		});
 		const textbox: HTMLElement = screen.getByRole('textbox');
 		await user.type(textbox, 'Mich');
 
 		const autocomplete = screen.getByTestId('autocomplete');
 		await new Promise((resolve) => setTimeout(resolve, 200)); // Await debounce
 		const suggestion = within(autocomplete).getByRole('button');
+
+		vi.unstubAllGlobals();
+
+		// add a mock feature for feature
+		const mockFeatureFetch = vi.fn().mockResolvedValue({
+			json: vi.fn().mockResolvedValue(mockFeature),
+			ok: true,
+			status: 200,
+			headers: { 'Content-Type': 'application/json' }
+		});
+		vi.stubGlobal('fetch', mockFeatureFetch);
+
 		await user.click(suggestion);
 		await tick();
+		expect(textbox).toHaveValue('Michigan Stadium');
 	});
 });
---
+++ src/lib/components/SearchBox/fixtures/SearchboxFixture.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Fragment from '$lib/components/Fragment/Fragment.svelte';
+	import SearchBox from '../SearchBox.svelte';
+
+	interface Props {
+		context?: Record<string, unknown>;
+	}
+
+	let { context }: Props = $props();
+</script>
+
+<Fragment {context}>
+	<SearchBox></SearchBox>
+</Fragment>
